5256650b013289f8579af6dee83d43890f881b9f
Justin Sorensen <sorensen.justin.j@gmail.com>
2015-02-11 14:03:50 -0700

    debuged arduino reciever

--- XYZ_control/XYZ_arduino/xyz_multi_motor_controller/xyz_multi_motor_controller.ino
+++ XYZ_control/XYZ_arduino/xyz_multi_motor_controller/xyz_multi_motor_controller.ino
@@ -1,8 +1,25 @@
 #include <AccelStepper.h>
 
-AccelStepper motor_x(AccelStepper::FULL2WIRE, 10, 11);
-AccelStepper motor_y(AccelStepper::FULL2WIRE, 12, 13);
-AccelStepper motor_z(AccelStepper::FULL2WIRE, 14, 15);
+int motorSpeed = 1200; //maximum steps per second (about 3rps / at 16 microsteps)
+int motorAccel = 80000;
+
+int motor_x_DirPin = 7; //digital pin 2
+int motor_x_StepPin = 8; //digital pin 3
+int motor_x_dir = -1; // reverse direction if need be
+
+int motor_y_DirPin = 5; //digital pin 2
+int motor_y_StepPin = 6; //digital pin 3
+int motor_y_dir = 1; // reverse direction if need be
+
+int motor_z_DirPin = 3; //digital pin 2
+int motor_z_StepPin = 4; //digital pin 3
+int motor_z_dir = 1; // reverse direction if need be
+
+AccelStepper motor_x(1, motor_x_StepPin, motor_x_DirPin);
+AccelStepper motor_y(1, motor_y_StepPin, motor_y_DirPin);
+AccelStepper motor_z(1, motor_z_StepPin, motor_z_DirPin);
+
+
 String incommingCommand;
 String commandString;
 int distance;
@@ -11,27 +28,19 @@ void processCommand(String in){
   commandString = in.substring(0,6);
   distance = in.substring(6).toInt();
   
-  //Serial.print( commandString + "\n");
-  if(commandString == "1:x:+:"){
-    Serial.print("positive x :" + String(distance) + "; position: " + String(motor_x.currentPosition()) + "\n");
-    motor_x.moveTo( distance + motor_x.currentPosition() );
-  } else if(commandString == "1:x:-:"){
-    Serial.print("negitive x " + String(distance) + "\n");
-    motor_x.moveTo( distance - motor_x.currentPosition() );
-  } else if(commandString == "1:y:+:"){
-    Serial.print("positive y " + String(distance) + "\n");
-    motor_y.moveTo( distance + motor_y.currentPosition() );
-  } else if(commandString == "1:y:-:"){
-    Serial.print("negitive y " + String(distance) + "\n");
-    motor_y.moveTo( distance - motor_y.currentPosition() );
-  } else if(commandString == "1:z:+:"){
-    Serial.print("positive z " + String(distance) + "\n");
-    motor_z.moveTo( distance + motor_z.currentPosition() );
-  } else if(commandString == "1:z:-:"){
-    Serial.print("negitive z " + String(distance) + "\n");
-    motor_z.moveTo( distance - motor_z.currentPosition() );
+  if(commandString == "1:x:+:"){          // X positive
+    motor_x.move( distance * motor_x_dir);
+  } else if(commandString == "1:x:-:"){   // X Negitive
+    motor_x.move( distance * -1 * motor_x_dir);
+  } else if(commandString == "1:y:+:"){   // Y Positive
+     motor_y.move( distance * motor_y_dir);
+  } else if(commandString == "1:y:-:"){   // Y Negitive
+     motor_y.move( distance * -1 * motor_y_dir);
+  } else if(commandString == "1:z:+:"){   // Z Positive
+     motor_z.move( distance * motor_z_dir);
+  } else if(commandString == "1:z:-:"){   // Z Negitive
+     motor_z.move( distance * -1 * motor_z_dir);
   } else {
-    Serial.print("Command Not Found\n");
   }
 }
 
@@ -51,14 +60,19 @@ void checkSerialCommand(){
 }
 
 void setup(){
-  motor_x.setMaxSpeed(1000);
-  motor_x.setSpeed(50);
+  motor_x.setMaxSpeed(motorSpeed);
+  motor_x.setSpeed(motorSpeed);
+  motor_x.setAcceleration(motorAccel);
+  //motor_x.moveTo(1000);
   
-  motor_y.setMaxSpeed(1000);
-  motor_y.setSpeed(50);
+  motor_y.setMaxSpeed(motorSpeed);
+  motor_y.setSpeed(motorSpeed);
+  motor_y.setAcceleration(motorAccel);
   
-  motor_z.setMaxSpeed(1000);
-  motor_z.setSpeed(50);
+  
+  motor_z.setMaxSpeed(motorSpeed);
+  motor_z.setSpeed(motorSpeed);
+  motor_z.setAcceleration(motorAccel);
   
   Serial.begin(9600);
   Serial.print("begin\n");
@@ -66,9 +80,9 @@ void setup(){
 
 void loop(){
   
- motor_x.runSpeed();
- motor_y.runSpeed();
- motor_z.runSpeed();
+ motor_x.run();
+ motor_y.run();
+ motor_z.run();
  
  checkSerialCommand();
  
