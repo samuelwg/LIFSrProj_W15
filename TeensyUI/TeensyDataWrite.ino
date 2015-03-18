04f7743b8b939e6648d835b7a43bc53accbe318b
Joshua Stapley <joshstapley_89@yahoo.com>
2015-03-18 12:38:16 -0600

    Updated TeensyUI

--- TeensyUI/TeensyDataWrite.ino
+++ TeensyUI/TeensyDataWrite.ino
@@ -5,13 +5,12 @@ IntervalTimer timer0;
 void SampleVoltage() {
   val = analogRead(0);
   val2 = analogRead(1);
-  unsigned char serialBytes[2];
+  unsigned char serialBytes[4];
   serialBytes[0] = (val >> 8) & 0xff;
   serialBytes[1] = val & 0xff;
-  Serial.write(serialBytes,2);
-  serialBytes[0] = (val2 >> 8) & 0xff;
-  serialBytes[1] = val2 & 0xff;
-  Serial.write(serialBytes,2);
+  serialBytes[2] = (val2 >> 8) & 0xff;
+  serialBytes[3] = val2 & 0xff;
+  Serial.write(serialBytes,4);
 }
 
 void timer_setup() {
